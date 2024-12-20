00c373eec0413f5e20ba54b35ec8020057488a9a
Tomáš Mach <52257177+tomasmach@users.noreply.github.com>
2024-12-20 01:54:24 +0100

    Fine, fixed it quickly

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 with open("books/frankenstein.txt") as f:
     file_contents = f.read()
-    
+
 
 
 def give_unique_letters(data):
     alphabet_count = {}
     for i in data:
-        if i.lower() in alphabet_count:
-            alphabet_count[i.lower()] += 1
-        else:
-            alphabet_count[i.lower()] = 1
-    
+        if i.isalpha():
+            if i.lower() in alphabet_count:
+                alphabet_count[i.lower()] += 1
+            else:
+                alphabet_count[i.lower()] = 1
+
     return alphabet_count
 
 def give_report(characters):
-    for j in characters:
-        print(f"The {j} character was found {characters[j]} times")
+    for char, count in characters:
+        print(f"The {char} character was found {count} times")
 
 character_counter = give_unique_letters(file_contents)
+sorted_characters = sorted(character_counter.items(), key=lambda x: x[1], reverse=True)
 
-print(character_counter)
-give_report(character_counter)
+give_report(sorted_characters)
